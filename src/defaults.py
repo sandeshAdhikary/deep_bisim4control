3513027b04fcf19c24c33e24c6e532be68cbf1f2
sandeshAdhikary <adhikary.sandesh@gmail.com>
2023-10-23 19:51:03 -0700

    env vars

--- src/defaults.py
+++ src/defaults.py
@@ -5,6 +5,7 @@ from envyaml import EnvYAML
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 with open(f'{PROJECT_DIR}/src/defaults.yaml') as f:
-    DEFAULTS = yaml.load(f, Loader=yaml.FullLoader)
+    # DEFAULTS = yaml.load(f, Loader=yaml.FullLoader)
+    DEFAULTS = dict(EnvYAML(f))
 
 DEFAULTS['project_dir'] = PROJECT_DIR
