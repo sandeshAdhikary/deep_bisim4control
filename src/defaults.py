79a92d9a341e103b7d72c6e9c885e637eb09b3ca
sandeshAdhikary <adhikary.sandesh@gmail.com>
2023-10-23 19:52:32 -0700

    fixed env var parsing from yaml files with envyaml

--- src/defaults.py
+++ src/defaults.py
@@ -4,8 +4,8 @@ from envyaml import EnvYAML
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-with open(f'{PROJECT_DIR}/src/defaults.yaml') as f:
+# with open(f'{PROJECT_DIR}/src/defaults.yaml') as f:
     # DEFAULTS = yaml.load(f, Loader=yaml.FullLoader)
-    DEFAULTS = dict(EnvYAML(f))
+DEFAULTS = dict(EnvYAML(f'{PROJECT_DIR}/src/defaults.yaml'))
 
 DEFAULTS['project_dir'] = PROJECT_DIR
