5a0a58fdf49e764b3020da7ffc975680b803d049
sandeshAdhikary <adhikary.sandesh@gmail.com>
2023-10-23 19:03:27 -0700

    cleaned up defgaults. added logdir to be pulled from env

--- src/defaults.py
+++ src/defaults.py
@@ -5,4 +5,5 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 with open(f'{PROJECT_DIR}/src/defaults.yaml') as f:
     DEFAULTS = yaml.load(f, Loader=yaml.FullLoader)
+
 DEFAULTS['project_dir'] = PROJECT_DIR
